c06768547c573297f2ad582561cf109dc8300cf7
Robert Elliot <rob@lidalia.org.uk>
2015-04-10 13:32:08 +0100

    Prevents script continuing on error

--- bugfix-release.sh
+++ bugfix-release.sh
@@ -1,3 +1,5 @@
+set -ex
+
 mvn -Psonatype-oss-release release:clean
 mvn -Psonatype-oss-release -Dgpg.passphrase="$GPG_PASSPHRASE" --batch-mode clean release:prepare -DautoVersionSubmodules=true
 mvn -Psonatype-oss-release -Dgpg.passphrase="$GPG_PASSPHRASE" release:perform
