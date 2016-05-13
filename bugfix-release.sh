7c8208a9ebb91e731b49d7c3e10ad5d839ecff3a
Robert <rob@lidalia.org.uk>
2016-05-13 11:21:55 +0100

    Adds Snap config

--- bugfix-release.sh
+++ bugfix-release.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 set -ex
+
+git config --global user.name $SNAP_STAGE_TRIGGERED_BY
+git config --global user.email $EMAIL
 
 mvn -Psonatype-oss-release release:clean
 mvn -Psonatype-oss-release -Dgpg.passphrase="$GPG_PASSPHRASE" --batch-mode clean release:prepare -DautoVersionSubmodules=true
